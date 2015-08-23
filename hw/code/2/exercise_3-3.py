f6397f6ee422e5e6aa191e1c14e293f6de7b93a6
Manish Singh <mrsingh@ncsu.edu>
2015-08-23 14:42:17 -0400

    solved exercise 3-3 . added exercise_3-3.py

---
+++ hw/code/2/exercise_3-3.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 23 14:34:13 2015
+
+@author: manish
+"""
+
+def right_justify(s):
+    print (" "*(70-len(s))+s)
+    
+
+right_justify('allen')
+    
+    
