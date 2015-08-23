fb10429a55707d5e6f7c9bb7b0666cc800c5a037
Manish Singh <mrsingh@ncsu.edu>
2015-08-23 13:27:11 -0400

    started code 2. just wrote the program for exercise 3-1. Going to modify next to solve 3-1

---
+++ hw/code/2/think3.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 23 13:23:31 2015
+
+@author: manish
+"""
+
+def print_lyrics():
+    print "I'm a lumberjack, and I'm okay."
+    print "I sleep all night and I work all day."
+    
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+    
+
+repeat_lyrics()
