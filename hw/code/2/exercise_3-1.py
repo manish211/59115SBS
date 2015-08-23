e8a4e2ed0a4b51f6e87da6882a58f49f9012f366
Manish Singh <mrsingh@ncsu.edu>
2015-08-23 13:34:44 -0400

    added solutions for exercise 3-1 and 3-2

--- hw/code/2/exercise_3-1.py
+++ hw/code/2/exercise_3-1.py
@@ -4,17 +4,14 @@ Created on Sun Aug 23 13:23:31 2015
 
 @author: manish
 """
+repeat_lyrics()
 
 def print_lyrics():
     print "I'm a lumberjack, and I'm okay."
-    print "I sleep all night and I work all day."    
-
+    print "I sleep all night and I work all day."
     
 def repeat_lyrics():
     print_lyrics()
     print_lyrics()
     
 
-    
-
-repeat_lyrics()
