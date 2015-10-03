2559176fe8fbfd85c9ff0fc4a87d2d787562b46e
mrsingh <mrsingh@ncsu.edu>
2015-10-03 15:38:36 -0400

    Some more runs

--- hw/code/4/code4.py
+++ hw/code/4/code4.py
@@ -1,17 +1,25 @@
 import random
-
+import math
 
 def getRandomState():
 	x = random.randrange(-10000,10000) 
 	return x
 
 def neighbor():
-	return getRandomState
+	randomNeighbor = getRandomState()
+	return randomNeighbor
 
 def getEnergyAtState(x):
+	# print x
+	# print "------"	
 	f1=pow(x,2)
 	f2=pow((x-2),2)
-	return f1+f2
+	energy = f1 + f2
+	if energy < 0:
+		print "energy negative:",energy
+		print "*****************************************"
+		print "*****************************************"
+	return energy 
 
 
 def getMinMax():
@@ -22,7 +30,7 @@ def getMinMax():
 	minEnergy=e
 	maxEnergy=e
 
-	while i in range(100):
+	for i in range(100000):
 		x=random.randrange(-10000,10000)	
 		
 		e=getEnergyAtState(x)
@@ -38,25 +46,36 @@ def getMinMax():
 
 def getBaseLinedEnergy(x):
 	e=getEnergyAtState(x)
-	baseLinedEnergy = float(e-minEnergy) / (maxEnergy - minEnergy)
-	return baseLinedEnergy
+	minEnergy,maxEnergy = getMinMax()
+	baseLinedEnergyVal = float(e-minEnergy) / (maxEnergy - minEnergy)
+	if baseLinedEnergyVal < 0:
+		print "======================================================"
+		print "minEnergy:",minEnergy," maxEnergy:",maxEnergy," energy e:",e," baseLinedEnergyVal:",baseLinedEnergyVal
+		print "======================================================"
+	return baseLinedEnergyVal
 
 def probOfAccept(old,new,temperature):
-	probability = pow(e,(old-new)/temperature)
+	probability = math.exp(float((old-new)/float(temperature)))
 	return probability
 
 
-def startSimAnn:
+def startSimAnn(emax):
+	print "started"
 	s0 = getRandomState();
 	e0 = getBaseLinedEnergy(s0);
 	s,e=s0,e0
 	sb,eb=s,e
-	k = 0
+	k = 100 
+	kmax = k
 
-	while k < kmax and e > emax:
-		sn = neighbor
+	while k > 0 and e > emax:
+		sn = neighbor()
+		# print "Got the neighbor+++"
+		# print sn
+		# print "====="
 		en = getBaseLinedEnergy(sn)
-
+		# print "Got the baseLinedEnergy++++"
+		print "s,e:",s," ",e," ","sb,eb:",sb," ",eb," ""sn,en:",sn," ",en," "
 		if (en < eb):
 			sb,eb = sn,en
 			print "!"
@@ -65,21 +84,31 @@ def startSimAnn:
 			s,e = sn,en
 			print "+"
 		else:
-			temperature = k/kmax
-			if probOfAccept(e,en,temperature) < rand():
+			# print "k:",;print k
+			# print "kmax:",;print kmax
+			temperature = float(k)/float(kmax)
+			# print "current temperature:",
+			# print temperature
+			if probOfAccept(e,en,temperature) > random.random(): 
 				s,e = sn,en
 				print "?"
 
 		print "."
 		
-		k = k + 1
+		k = k - 1
+		print "reduced k man:",
+		print k," sb :",sb, " eb :",eb
+		# if eb < 0:
+		# 	print "+++++++++++++++++"
+		# 	print "+++++++++++++++++"
 		
 		if k % 50 == 0:
 			print "\n",sb
 		
-		return sb
+	return sb
 
 
 if __name__ == "__main__":
 	print "Starting Simulated Annealer : Hold on tight!"
-	print "Best Solution is: "+startSimAnn
+	emax = 0.000000001
+	startSimAnn(emax)
