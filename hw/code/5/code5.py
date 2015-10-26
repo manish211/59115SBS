cede26c92b5b618612fc869e8cdc7c007fef8c8f
mrsingh <mrsingh@ncsu.edu>
2015-10-25 21:43:25 -0400

    Added variable bound checker

--- hw/code/5/code5.py
+++ hw/code/5/code5.py
@@ -2,11 +2,28 @@ import random
 import math
 
 def  var_bound_checker(candidate = []):
-	for x in candidate:
-		print x
+	true_count = 0
 
+	if(0 <= candidate[0]) and (candidate[0] <= 10):
+		true_count += 1;
 
+	if(0 <= candidate[1]) and (candidate[1] <= 10):
+		true_count += 1;
+	
+	if(1 <= candidate[2]) and (candidate[2] <= 5):
+		true_count += 1;
+	
+	if(1 <= candidate[4]) and (candidate[5] <= 5):
+		true_count += 1;
 
-candidate1=[1,2,3,4,5]
+	if(0 <= candidate[3]) and (candidate[3] <= 6):
+		true_count += 1;
 
-var_bound_checker(candidate1)
+	if(0 <= candidate[5]) and (candidate[5] <= 10):
+		true_count += 1;
+
+	if( true_count == 6):
+		return True
+	else
+		return False
+	
