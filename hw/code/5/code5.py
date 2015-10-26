5493e569ff5253c4d514f2246f80dadd176d871a
mrsingh <mrsingh@ncsu.edu>
2015-10-25 22:55:03 -0400

    Added constraint checker

--- hw/code/5/code5.py
+++ hw/code/5/code5.py
@@ -26,4 +26,65 @@ def  var_bound_checker(candidate = []):
 		return True
 	else
 		return False
-	
+
+
+def constraint_checker(candidate = []):
+	true_count = 0
+
+	make_square = lambda x: math.pow(x,2)
+
+	make_cube = lambda x: math.pow(x,3)
+
+	constraints = []
+
+	constraints[0] = candidate[0] + candidate[1] -2
+
+	constraints[1] = 6 - candidate[0] - candidate[1]
+
+	constraints[2] = 2 - candidate[1] + candidate[0]
+
+	constraints[3] = 2 - candidate[0] + 3*candidate[1]
+
+	constraints[4] = 4 - make_square(candidate[2] - 3) - candidate[3]
+
+	constraints[5] = make_cube(candidate[4] - 3) + candidate[5] - 4
+
+	if ( 0 <= constraints[0] ) and ( 0 <= constraints[1] ) 
+	and ( 0 <= constraints[2] ) and ( 0 <= constraints[3] )
+	and ( 0 <= constraints[4] ) and ( 0 <= constraints[5] ):
+		return True
+	else
+		return False
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
